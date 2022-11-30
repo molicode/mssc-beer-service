955fd5c37b5906a037ee521c3322a454939e4305
Luis Molina <molicode1347@gmail.com>
2022-11-30 18:23:36 -0300

    adding web data model. closes #2

---
+++ src/main/java/molicode/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,15 @@
+package molicode.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON
+
+}
