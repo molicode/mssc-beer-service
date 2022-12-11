547499802e5db33b2d74e1d60571738755742d88
Luis Molina <molicode1347@gmail.com>
2022-12-11 15:48:22 -0300

    adding beer controller test. closes #4

--- src/main/java/molicode/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/molicode/springframework/msscbeerservice/web/controller/BeerController.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import molicode.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
